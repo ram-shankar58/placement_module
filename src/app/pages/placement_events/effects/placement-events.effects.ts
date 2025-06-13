f43f81dd324951f74410c51ca17bd340fff90ec5
ram-shankar58 <sanjayramshankar@gmail.com>
2025-06-13 16:29:14 +0530

    placement event effects to be done

---
+++ src/app/pages/placement_events/effects/placement-events.effects.ts
@@ -0,0 +1,17 @@
+//LEFT TO BE IMPLEMENTED SOON
+
+// import { Injectable, OnDestroy, onDestroy } from '@angular/core';
+// import { Actions, createEffect, ofType } from '@ngrx/effects';
+// import { Store } from '@ngrx/store';
+// import { catchError, map, switchMap } from 'rxjs/operators';
+// import { of, Subscription } from 'rxjs';
+// import * as PlacementEventsAction from '../action/placement_events.action';
+// import { PlacementEventsService } from '../placement-events.service';
+
+// @Injectable({providedIn: 'root'})
+
+// export class PlacementEventsEffect implements OnDestroy{
+//     private subscriptions: Subscription = new Subscription();
+
+//     constructor(private actions$: Actions, protected placementEventsService: P)
+// }
